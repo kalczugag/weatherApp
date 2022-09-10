21a10b41dd8b84ef5900c32f5304c1af17cbc959
kalczugag <kalczugag@gmail.com>
2022-09-10 17:56:42 +0200

    final project without working geolocation

--- scripts/script.js
+++ scripts/script.js
@@ -4,21 +4,38 @@ const nLoading = document.querySelector(".notification-container-loading");
 const nError = document.querySelector(".notification-container-error");
 const inputS = document.querySelector(".input-section");
 const buttonS = document.querySelector(".button-section");
+const returnBtn = document.querySelector(".return-Button");
 const orText = document.querySelector(".or-text");
 const weatherInfo = document.querySelector(".weather-info");
 const weatherInfoFooter = document.querySelector(".weather-info-footer");
 
 el.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    if (el.value.length < 1) {
-      document.querySelector(
-        ".notification-container-error"
-      ).innerHTML = `${el.value} isn't a valid city name`;
+    nError.style.display = "none";
+    returnBtn.onclick = () => {
+      inputS.style.display = "block";
+      buttonS.style.display = "block";
+      orText.style.display = "block";
+      weatherInfo.style.display = "none";
+      weatherInfoFooter.style.display = "none";
 
-      return (document.querySelector(
-        ".notification-container-error"
-      ).style.display = "block");
+      el.value = "";
+    };
+
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+          return pos.coords.latitude, pos.coords.longitude;
+        });
+      } else {
+        return (nError.innerHTML =
+          "Geolocation is not supported by this browser.");
+      }
     }
+    document.querySelector(".get-location-button").onclick = () => {
+      getLocation();
+    };
+
     const options = {
       method: "GET",
       headers: {
@@ -28,7 +45,9 @@ el.addEventListener("keydown", function (event) {
     };
 
     fetch(
-      `https://weatherapi-com.p.rapidapi.com/current.json?q=${el.value}`,
+      `https://weatherapi-com.p.rapidapi.com/current.json?q=${
+        el.value || getLocation()
+      }`,
       options
     )
       .then((response) => response.json())
@@ -36,36 +55,60 @@ el.addEventListener("keydown", function (event) {
         console.log(response);
         wrapperChange(response);
       })
-      .catch((err) => {
-        console.log(err);
-        notifications(err);
-      });
+      .catch((err) => onError(err));
   }
 });
 
-const wrapperChange = (resp) => {
-  if (resp.ok) {
-    nLoading.style.display = "none";
-    nError.style.display = "none";
-    inputS.style.display = "none";
-    buttonS.style.display = "none";
-    orText.style.display = "none";
-    weatherInfo.style.display = "flex";
+const tempIcon = document.querySelector(".temp-icon");
 
-    if ((resp.current.condition = "overcast")) {
+const wrapperChange = (resp) => {
+  if ((el.value = resp.location.name)) {
+    nLoading.style.display = "block";
+    setTimeout(() => {
+      nLoading.style.display = "none";
+      nError.style.display = "none";
+      inputS.style.display = "none";
+      buttonS.style.display = "none";
+      orText.style.display = "none";
+      weatherInfo.style.display = "flex";
+
       document.querySelector(
         ".cloud-image"
-      ).innerHTML = `<img src="weatherIcons/cloud.svg" width=110>`;
-    }
-    document.querySelector(".temp").innerHTML = `${resp.current.temp_c}&deg;C`;
-    //   document.querySelector(".cloud-description").innerHTML =
-    resp.current.condition;
-    weatherInfoFooter.style.display = "flex";
-    document.querySelector(".humidity-percent").innerHTML = resp.current.text;
+      ).innerHTML = `<img src="${resp.current.condition.icon}" width=110>`;
+
+      if (resp.current.temp_c <= 0) {
+        tempIcon.innerHTML = `<i class="fa fa-thermometer-empty" aria-hidden="true"></i>`;
+      } else if (resp.current.temp_c <= 5) {
+        tempIcon.innerHTML = `<i class="fa fa-thermometer-quarter" aria-hidden="true"></i>`;
+      } else if (resp.current.temp_c <= 15) {
+        tempIcon.innerHTML = `<i class="fa fa-thermometer-half" aria-hidden="true"></i>`;
+      } else if (resp.current.temp_c <= 25) {
+        tempIcon.innerHTML = `<i class="fa fa-thermometer-three-quarters" aria-hidden="true"></i>`;
+      } else if (resp.current.temp_c > 25) {
+        tempIcon.innerHTML = `<i class="fa fa-thermometer-full" aria-hidden="true"></i>`;
+      }
+
+      document.querySelector(
+        ".temp"
+      ).innerHTML = `${resp.current.temp_c}&deg;C`;
+      document.querySelector(".cloud-description").innerHTML =
+        resp.current.condition.text;
+      document.querySelector(
+        ".city-name"
+      ).innerHTML = `${resp.location.name}, ${resp.location.country}`;
+
+      weatherInfoFooter.style.display = "flex";
+      document.querySelector(
+        ".degree"
+      ).innerHTML = `${resp.current.feelslike_c}&deg;C`;
+      document.querySelector(
+        ".humidity-percent"
+      ).innerHTML = `${resp.current.humidity}%`;
+    }, 3000);
   }
 };
 
-const notifications = (err) => {
+function onError() {
   nError.style.display = "block";
-  return (nError.innerHTML = "No matching location found");
-};
+  nError.innerText = "No matching location found";
+}
